e0e409fac844bf7281fe23d38c626a9845279b7a
dhirajataws <dhirajataws@gmail.com>
2018-07-28 23:19:26 +1000

    next

--- search_car/src/App.js
+++ search_car/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         </header>
       </div>
         <SearchNavBar criteria={this.state.criteria} handleSearchClick={this.handleClick}/>
-        <Cars/>
+        <Cars criteria={this.state.criteria} />
       </div>
     );
   }
--- search_car/src/components/Cars.js
+++ search_car/src/components/Cars.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios'
+
 class Cars extends React.Component {
   state = {cars: [], loaded: false}
-  fetchCars= () => {
+  fetchCars = () => {
     axios({
       url: 'http://localhost:3000/cars',
       method: 'get',
@@ -21,76 +22,40 @@ class Cars extends React.Component {
       },
     )
   }
+
   componentDidMount() {
     this.fetchCars()
   }
-  // componentDidUpdate(prevProps) {
-  //   if (
-  //     prevProps.username !== this.props.username
-  //   ) {
-  //     this.fetchCompany()
-  //   }
-  // }
+
   render() {
-    console.log(this.state.cars)
-
-    return(
+    return (
       this.state.loaded ?
-        this.state.cars.map((car,index) => (
-
-                  <div className="row">
-                    <div className="col1 col-xs-12 col-md-4">
-                      {car.id}
-                    </div>
-                    <div className="col2 col-xs-12 col-md-4">
-                      {car.price}
-                      </div>
-                    <div className="col3 col-xs-12 col-md-4">
-                      {car.imageUrl}
-                      </div>
-                </div>
-
-                // <div>{car.id}</div>
-        ))
-        :'...'
-      //   this.state.cars.map(car => {
-      //       <div>{car.id}</div>
-      //       })
-        // :'...'
+        (
+          <div className="container">
+            {this.state.cars.filter(item => this.props.criteria ? item.name === this.props.criteria : true)
+              .map((car, index) => (<Car carvalue={car}/>))}
+          </div>
+        )
+        : '...'
     )
-    // return this.state.loaded
-    //   ? this.state.error
-    //     ? 'ERROR (You probably need to add your own token)'
-    //     : this.state.cars.map(car => {
-    //     <div>{car.id}</div>
-    //     })||'Unknown'
-    //   : '...'
   }
 }
+
 export default Cars;
 
-const Car =(props)=>{
-  console.log(props.carvalue)
-
-  return(
-    <div className="container">
-          <div className="row">
-            <div className="col1 col-xs-12 col-md-4">
-              {props.carvalue.id}
-            </div>
-            <div className="col2 col-xs-12 col-md-4">
-              {props.carvalue.price}
-              </div>
-            <div className="col3 col-xs-12 col-md-4">
-              {props.carvalue.imageUrl}
-              </div>
-        </div>
+const Car = (props) => {
+  return (
+    <div className="row">
+      <div className="col1 col-xs-12 col-md-2">
+        {props.carvalue.name}
+      </div>
+      <div className="col1 col-xs-12 col-md-2">
+        {props.carvalue.price}
+      </div>
+      <div className="col3 col-xs-12 col-md-8">
+        {props.carvalue.imageUrl}
+      </div>
     </div>
   )
-
-
-  //   <Car carvalue={car}/>
-
-
 }
 
